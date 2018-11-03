cb78d6bcfcc9b57982a740f453f24b277ec9d66b
bokwoon <bokwoon.c@gmail.com>
2018-11-03 21:27:59 +0800

    WEW in the middle of project 2

--- project2.ml
+++ project2.ml
@@ -30,6 +30,9 @@ let alphanumerals = [
 ;;
 
 let words str =
+  let alphanumerals = [
+    'a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z'; 'A'; 'B'; 'C'; 'D'; 'E'; 'F'; 'G'; 'H'; 'I'; 'J'; 'K'; 'L'; 'M'; 'N'; 'O'; 'P'; 'Q'; 'R'; 'S'; 'T'; 'U'; 'V'; 'W'; 'X'; 'Y'; 'Z'; '0'; '1'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9';
+  ] in
   let wordbuf = Buffer.create 16 in
   let str_len = String.length str in
   let rec aux i accu =
@@ -155,10 +158,14 @@ let r = compute_distribution ["a";"b";"c";"b";"c";"a";"b";"c";"c";"c"];;
 
 let introspect htable =
   Hashtbl.fold (fun k v acc -> (k, v) :: acc) htable [];;
-let make_htable list =
-let rec aux 
-  match list with
+
+let make_htable (list:('a*'b) list) =
+  let htable = Hashtbl.create 16 in
+  List.fold_left (fun () (k,v) -> Hashtbl.add htable k v) () list;
+  htable
 ;;
+let ht = make_htable [ ("a",1); ("b",2); ("c",3); ("d",4); ("e",5); ("f",6); ("g",7); ("h",8); ("i",9); ("j",10); ];;
+introspect ht;;
 
 let update_alist alist word payload_f default =
   let rec aux alist_dc accu =
@@ -169,6 +176,7 @@ let update_alist alist word payload_f default =
   in
   aux alist []
 ;;
+
 let sample_alist = [("a",1);("b",1);("c",1);("d",1);("e",1);("f",1);];;
 let r = update_alist sample_alist "b" (fun x->x+1) 1;;
 let r = update_alist sample_alist "z" (fun x->x+1) 1;;
@@ -184,18 +192,18 @@ let hashtbl_find_opt htable item =
   | Not_found -> None
 ;;
 let ht = Hashtbl.create 16;;
-Hashtbl.add ht "a" 1;;
+Hashtbl.replace ht "a" 99;;
 
 let update_htable htable word payload =
   let var_opt = hashtbl_find_opt htable word
   in
   let num,alist = match var_opt with
-    | None -> 1,[]
+    | None -> 0,[]
     | Some {total=a; amounts=alist} -> a,alist
   in
-  let alist_new = update_alist alist word (fun a->a+1) 1
+  let alist_new = update_alist alist payload (fun a->a+1) 1
   in
-  Hashtbl.replace htable word {total=num; amounts=alist_new}
+  Hashtbl.replace htable word {total=(num+1); amounts=alist_new}
 ;;
 
 let build_htable words =
@@ -218,12 +226,207 @@ let ht = build_htable ts_lis;;
 let r = introspect ht;;
 
 (* Define next_in_htable : htable -> string -> string that does the same thing as next_in_ltable for the new table format. *)
-let next_in_htable table word =
-  "Replace this string with your implementation." ;;
+let next_in_htable htable word =
+  try
+    let {total=num; amounts=alist} = Hashtbl.find htable word in
+    let rand_idx = (Random.int num) + 1 in
+    let rec aux i l =
+      match l with
+      | [] -> failwith "next_in_htable.aux: reached the end of the list but rand_idx is still larger than 0"
+      | (k,v)::t -> (let i2 = i - v in
+                     if i2 <= 0 then k
+                     else
+                       aux i2 t
+                    )
+    in
+    aux rand_idx alist
+  with
+  | Not_found -> "word not found in htable"
+;;
 
 (* Finally, define walk_htable : htable -> string list *)
-let walk table =
-  "Replace this string with your implementation." ;;
 
-let walk_htable table =
-  "Replace this string with your implementation." ;;
+let walk_htable (htable:htable) : string list =
+  let rec aux accu input =
+    let output = next_in_htable htable input in
+    match output with
+    | "STOP" -> List.rev accu
+    | _ -> aux (output::accu) output
+  in
+  aux [] "START"
+;;
+
+(* PART C: QUALITY IMPROVEMENTS *)
+(* If we want to generate sentences from larger corpuses, such as the ones of the ebooks_corpus given in the prelude, we cannot just ignore the punctuation. We also want to generate text using not only the beginning of the original text, but the start of any sentence in the text. *)
+(* Define sentences : string -> string list list that splits a string into a list of sentences such as: *)
+(* uninterrupted sequences of roman letters, numbers, and non ASCII characters (in the range '\128'..'\255') are words; *)
+(* single punctuation characters ';', ',', ':', '-', '"', '\'', '?', '!' and '.' are words; *)
+(* punctuation characters '?', '!' and '.' terminate sentences; *)
+(* everything else is a separator; *)
+(* and your function should not return any empty sentence. *)
+
+let alphanumeral_q char =
+  let int = int_of_char char in
+  if int >= (int_of_char '0') && int <= (int_of_char '9')
+  || int >= (int_of_char 'a') && int <= (int_of_char 'z')
+  || int >= (int_of_char 'A') && int <= (int_of_char 'Z')
+  then
+    true
+  else
+    false
+;;
+
+let punctuation_q char =
+  let int = int_of_char char in
+  if int = (int_of_char ';')
+  || int = (int_of_char ',')
+  || int = (int_of_char '-')
+  || int = (int_of_char '\'')
+  || int = (int_of_char '?')
+  || int = (int_of_char '!')
+  || int = (int_of_char '.')
+  then
+    true
+  else
+    false
+;;
+
+let terminator_q char =
+  let int = int_of_char char in
+  if int = (int_of_char '?')
+  || int = (int_of_char '!')
+  || int = (int_of_char '.')
+  then
+    true
+  else
+    false
+;;
+
+let separator_q char =
+  not (alphanumeral_q char || punctuation_q char);;
+
+let flush_properly char word_buf word_list sentence_list =
+  let word_buf_not_empty = Buffer.length word_buf != 0 in
+  let word = Buffer.contents word_buf in
+  let strchar = String.make 1 char in
+
+  if terminator_q char
+  then
+    if word_buf_not_empty
+    then
+      let () = Buffer.clear word_buf in
+      let word_list = strchar::word::word_list in
+      let sentence_list = (List.rev word_list)::sentence_list in
+      let word_list = [] in
+      word_list,sentence_list
+    else
+      let word_list = strchar::word_list in
+      let sentence_list = (List.rev word_list)::sentence_list in
+      let word_list = [] in
+      word_list,sentence_list
+
+  else if punctuation_q char
+  then
+    if word_buf_not_empty
+    then
+      let () = Buffer.clear word_buf in
+      let word_list = strchar::word::word_list in
+      word_list,sentence_list
+    else
+      let word_list = strchar::word_list in
+      word_list,sentence_list
+
+  else if alphanumeral_q char
+  then
+    let () = Buffer.add_char word_buf char in
+    word_list,sentence_list
+
+  else
+  if word_buf_not_empty
+  then
+    let () = Buffer.clear word_buf in
+    let word_list = word::word_list in
+    word_list,sentence_list
+  else
+    word_list,sentence_list
+;;
+
+let flush_properly char word_buf word_list sentence_list =
+  if alphanumeral_q char
+  then
+    let () = Buffer.add_char word_buf char in
+    word_list,sentence_list
+  else
+
+    let word_list =
+      if Buffer.length word_buf != 0
+      then
+        let word = Buffer.contents word_buf in
+        let () = Buffer.clear word_buf in
+        word::word_list
+      else
+        word_list
+    in
+
+    let word_list =
+      if punctuation_q char
+      then
+        (String.make 1 char)::word_list
+      else
+        word_list
+    in
+
+    let word_list,sentence_list =
+      if terminator_q char
+      then
+        let sentence = List.rev word_list in
+        let word_list = [] in
+        word_list,sentence::sentence_list
+      else
+        word_list,sentence_list
+    in
+
+    word_list,sentence_list
+;;
+
+let wb = Buffer.create 16;;
+Buffer.clear wb;Buffer.add_string wb "yeet";;
+let wl = ["twenty";"nineteen";"eighteen";"seventeen";"sixteen";];;
+let sl = [
+  ["eleven";"twelve";"thirteen";"fourteen";"fifteen";];
+  ["six";"seven";"eight";"nine";"ten";];
+  ["one";"two";"three";"four";"five";];
+];;
+let wl,sl = flush_properly 'a' wb wl sl;;
+let wl,sl = flush_properly ';' wb wl sl;;
+let wl,sl = flush_properly 'b' wb wl sl;;
+let wl,sl = flush_properly 'c' wb wl sl;;
+let wl,sl = flush_properly 'd' wb wl sl;;
+let wl,sl = flush_properly '.' wb wl sl;;
+Buffer.contents wb;;
+
+let sentences str =
+  let word_buf = Buffer.create 16 in
+  let story_buf = Buffer.create 16 in
+  let () = Buffer.add_string story_buf str in
+  let story_len = Buffer.length story_buf
+  in
+  let rec aux i word_list sentence_list =
+    if i = story_len
+    then
+      let word_list,sentence_list
+        = flush_properly ' ' word_buf word_list sentence_list in
+      (* (List.rev (List.rev word_list)::sentence_list) *)
+      (* (List.rev word_list)::sentence_list *)
+      List.rev sentence_list
+    else
+      let char = Buffer.nth story_buf i in
+      let word_list,sentence_list =
+        flush_properly char word_buf word_list sentence_list in
+      aux (i+1) word_list sentence_list
+  in
+  aux 0 [] []
+;;
+
+let sntnc = "This is a\nsample, albeit short, sentence. That should spawn three--! sentences.";;
+sentences sntnc;;
