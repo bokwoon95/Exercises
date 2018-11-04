ef50f6c8b651c29c4973d48843df459e2d79b6a9
bokwoon <bokwoon.c@gmail.com>
2018-11-04 21:57:02 +0800

    Starting the final week exercises

--- funmooc.ml
+++ funmooc.ml
@@ -1936,3 +1936,79 @@ let read_lines =
   in
   fun () -> aux ()
 ;;
+
+(* OPENING MODULES
+ * The code given in the template has been written using some values declared in three modules of the standard library. Can you find what are these three modules? When you have found them, just use open directives to have the code accepted by the type checker. Be aware that the iter function appears in multiple module and that the order in which you open the modules is important. *)
+open String;;
+open List;;
+open Digest;;
+let print_hashes (hashes : Digest.t list) : unit =
+  let print_hash h = h |> to_hex |> uppercase |> print_endline in
+  iter print_hash hashes;;
+
+(* ACCESSING MODULES AND SUBMODULES
+ * Use fully-qualified names to fix the compilation of [bfs]. (The open directive is forbidden here.) *)
+
+(* THE GIVEN PRELUDE *)
+module Tree = struct
+
+  type 'a t = Leaf of 'a | Node of 'a t * 'a * 'a t
+
+  module Iterator = struct
+
+    type 'a path =
+      | Top
+      | Left of 'a path * 'a * 'a t
+      | Right of 'a t * 'a * 'a path
+
+    type 'a iterator = Loc of 'a t * 'a path
+
+    exception Fail
+
+    let go_left (Loc (t, p)) =
+      match p with
+      | Top -> raise Fail
+      | Left (father, x, right) -> raise Fail
+      | Right (left, x, father) -> Loc (left, Left (father, x, t))
+
+    let go_right (Loc (t, p)) =
+      match p with
+      | Top -> raise Fail
+      | Left (father, x, right) -> Loc (right, Right (t, x, father))
+      | Right (left, x, father) -> raise Fail
+
+    let go_up (Loc (t, p)) =
+      match p with
+      | Top -> raise Fail
+      | Left(father, x, right) -> Loc (Node (t, x, right), father)
+      | Right(left, x, father) -> Loc (Node (left, x, t), father)
+
+    let go_first (Loc (t, p)) =
+      match t with
+      | Leaf _ -> raise Fail
+      | Node (left, x, right) -> Loc (left, Left (p, x, right))
+
+    let go_second (Loc (t, p)) =
+      match t with
+      | Leaf _ -> raise Fail
+      | Node (left, x, right) -> Loc (right, Right (left, x, p))
+
+    let focus (Loc ((Leaf x | Node (_, x, _)), _)) = x
+
+  end
+
+end
+
+(* YOUR ANSWER *)
+let bfs t =
+  let rec aux results = function
+    | [] ->
+       List.rev results
+    | l :: ls ->
+       let results = (focus l) :: results in
+       try
+         aux results (ls @ [ go_first l; go_second l])
+       with Fail ->
+         aux results ls
+  in
+  aux [] [Loc (t, Top)]
